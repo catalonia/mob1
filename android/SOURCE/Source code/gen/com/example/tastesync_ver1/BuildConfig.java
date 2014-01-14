09d40ddf716dfe3fc35f4429edfff26fef2de4d7
quangdao <duykhanh.t2@gmail.com>
2014-01-14 15:43:12 +0700

    Init android code
    Create android code with login facebook feature and Ask screen

---
+++ android/SOURCE/Source code/gen/com/example/tastesync_ver1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.tastesync_ver1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ android/SOURCE/Source code/src/com/example/tastesync/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.tastesync;
+
+import com.example.tastesync_ver1.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_tab_ask);
+	}
+
+}
